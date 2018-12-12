a29943d621639fa14e5ab2adb96914f602e0f9ba
amefan <1257096364@qq.com>
2018-12-12 10:15:05 +0800

    微服务springcloud学习项目搭建

---
+++ microservicecloud-api/src/main/java/com/afan/springcloud/entity/Dept.java
@@ -0,0 +1,27 @@
+package com.afan.springcloud.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * @ClassName: Dept
+ * @Description: 部门实体类
+ * @Author：Administrator
+ * @Date : 2018/12/12 9:34
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Accessors(chain = true)
+class Dept implements Serializable{
+
+    public Long deptno;
+    public String dname;
+    public String db_source;
+
+
+}
